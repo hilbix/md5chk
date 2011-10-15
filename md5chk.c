43e289f64b8472272df9232f5a6066d95fb0cc7b
Valentin Hilbig <webmaster@scylla-charybdis.com>
2011-10-15 07:12:02 +0000

    cat, overlap, maxsize

--- md5chk.c
+++ md5chk.c
@@ -10,7 +10,7 @@
  *	...
  * done
  *
- * Copyright (C)2006-2008 Valentin Hilbig <webmaster@scylla-charybdis.com>
+ * Copyright (C)2006-2011 Valentin Hilbig <webmaster@scylla-charybdis.com>
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -27,6 +27,9 @@
  * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
  *
  * $Log$
+ * Revision 1.14  2011-10-15 07:12:02  tino
+ * cat, overlap, maxsize
+ *
  * Revision 1.13  2009-07-17 01:10:17  tino
  * Library fixes
  *
@@ -73,37 +76,107 @@
 static unsigned char	tchar;
 static int		nflag, unbuffered, quiet, stdinflag, direct, zero;
 static int		ignore, errs;
+static int		overlap;
 static const char	*prefix;
+static unsigned long long	maxsize;
+static int		cat;
+
+static FILE		*out;
+
+static tino_md5_ctx	ctx[3];
+static int		effort;	/* additional contexts calculated: 0-2	*/
 
 static void
-md5init(tino_md5_ctx *ctx)
+md5init(int i)
 {
-  tino_md5_init(ctx);
+  tino_md5_init(ctx+i);
   if (prefix)
-    tino_md5_update(ctx, prefix, strlen(prefix));
+    tino_md5_update(ctx+i, prefix, strlen(prefix));
 }
 
 static void
-md5read(FILE *fd, unsigned char digest[16])
+md5upd(const void *ptr, size_t len)
 {
-  tino_md5_ctx	ctx;
+  int	i;
+
+  for (i=effort; i>=0; i--)
+    tino_md5_update(ctx+i, ptr, len);
+  if (cat)
+    {
+      fwrite(ptr, 1, len, stdout);
+      if (unbuffered)
+	fflush(stdout);
+    }
+}
+
+static void
+md5exit(int n)
+{
+  unsigned char	digest[16];
+  int i;
+
+  tino_md5_final(ctx+n, digest);
+  for (i=0; i<16; i++)
+    fprintf(out, "%02x", digest[i]);
+  if (unbuffered)
+    fflush(out);
+}
+
+static void
+md5copy(int from, int to)
+{
+  memcpy(ctx+to, ctx+from, sizeof *ctx);
+}
+
+/* A desparate EOF checker.
+ * Looks 1 character ahead.
+ */
+static int
+fd_at_eof(FILE *fd)
+{
+  int	c;
+
+  c = fgetc(fd);
+  if (c==EOF)
+    return 1;
+  ungetc(c, fd);
+  return 0;
+}
+
+/* returns true if another block must be read	*/
+static int
+md5read(FILE *fd)
+{
   char		data[BUFSIZ*10];
   int		got;
+  int		blk;
+  unsigned long long	len;
 
-  md5init(&ctx);
-  while ((got=fread(data, 1, sizeof data, fd))>0)
-    tino_md5_update(&ctx, data, got);
-  tino_md5_final(&ctx, digest);
+  len = maxsize;
+  blk = sizeof data;
+  if (len && blk>len)
+    blk = len;
+  while (blk && (got=fread(data, 1, blk, fd))>0)
+    {
+      md5upd(data, got);
+      if (!maxsize)
+	continue;
+      len -= got;
+      if (blk>len)
+	blk = len;
+    }
+  if (blk)
+    return 0;	/* EOF	*/
+  return !fd_at_eof(fd);
 }
 
 static void
-md5str(const char *str, unsigned char digest[16])
+md5str(const char *str)
 {
-  tino_md5_ctx	ctx;
-
-  md5init(&ctx);
-  tino_md5_update(&ctx, str, strlen(str));
-  tino_md5_final(&ctx, digest);
+  effort = 0;
+  md5init(0);
+  md5upd(str, strlen(str));
+  md5exit(0);
 }
 
 static void
@@ -111,28 +184,51 @@ shellescapename(const char *s)
 {
   for (; *s; s++)
     if (((unsigned char)*s)<33 || ((unsigned char)*s)>=127)
-      printf("\\%03o", (unsigned char)*s);
+      fprintf(out, "\\%03o", (unsigned char)*s);
     else
       switch (*s)
 	{
 	case '\\':
 	case '\'':
-	  putchar('\\');
+	  fputc('\\', out);
 	default:
-	  putchar(*s);
+	  fputc(*s, out);
 	  break;
 	}
 }
 
 static void
+term(void)
+{
+  if (zero)
+    fputc(0, out);
+  else
+    fputc('\n', out);
+  if (unbuffered)
+    fflush(out);
+}
+
+static void
+errterm(void)
+{
+  int e;
+
+  if (!effort)
+    return;
+  e = errno;
+  fprintf(out, "[ERR]");
+  term();
+  errno = e;
+}
+
+
+static void
 md5(const char *name)
 {
-  unsigned char	digest[16];
-  int		i;
-  FILE		*fd;
+  FILE	*fd;
 
   if (direct)
-    md5str(name, digest);
+    md5str(name);
   else
     {
       if (stdinflag && !strcmp(name, "-"))
@@ -142,35 +238,87 @@ md5(const char *name)
 	  tino_err("cannot open: %s", name);
 	  return;
 	}
-      md5read(fd, digest);
+      effort = 0;
+      md5init(0);
+      while (md5read(fd))
+	{
+	  if (!effort)
+	    {
+	      /* first block of -m
+	       */
+	      effort = 1;
+	      /* copy the first block so we can output it
+	       */
+	      md5copy(0,1);
+	      md5exit(1);
+	      md5init(1);
+	      continue;
+	    }
+
+	  /* already saw output	*/
+	  if (!overlap)
+	    {
+	      /* append next block	*/
+	      fputc('+', out);
+	      md5exit(1);
+	      md5init(1);
+	      continue;
+	    }
+
+	  /* overlapping case	*/
+	  if (effort==1)
+	    {
+	      /* first 2 blocks	*/
+	      effort = 2;
+	      md5copy(0,2);
+	    }
+
+	  fputc('-', out);
+	  md5exit(2);
+	  md5copy(1,2);
+	  md5init(1);
+	  continue;
+	}
       if (ferror(fd))
 	{
+	  errterm();
 	  tino_err("read error: %s", name);
 	  fclose(fd);
 	  return;
 	}
       if (fclose(fd))
 	{
+	  errterm();
 	  tino_err("cannot close: %s", name);
 	  return;
 	}
+      if (effort)
+	{
+	  /* already saw output	*/
+	  if (effort==2)
+	    {
+	      fputc('-', out);
+	      md5exit(2);
+	    }
+	  /* special degraded case is handled correctly, too:
+	   * 12 => 1+2=12 ("+2=" is output next)
+	   */
+	  fputc('+', out);
+	  md5exit(1);
+	  fputc('=', out);
+	}
+      /* output complete hash	*/
+      md5exit(0);
     }
-  for (i=0; i<sizeof digest; i++)
-    printf("%02x", digest[i]);
   if (!quiet)
     {
-      printf(" ");
+      fputc(' ', out);
       if (zero)
-	printf("%s", name);
+	fprintf(out, "%s", name);
       else
 	shellescapename(name);
     }
-  if (zero)
-    putchar(0);
-  else
-    putchar('\n');
-  if (unbuffered)
-    fflush(stdout);
+  term();
 }
 
 static void
@@ -259,12 +407,30 @@ main(int argc, char **argv)
 		      ,
 
 		      TINO_GETOPT_FLAG
+		      "c	Cat mode, echo input to stdout again\n"
+		      "		sends MD5 sum to stderr, use -u to use 2>&1"
+		      , &cat,
+
+		      TINO_GETOPT_FLAG
 		      "d	Do md5sum of commandline args or lines from stdin"
 		      , &direct,
 
 		      TINO_GETOPT_FLAG
 		      "i	ignore errors silently"
 		      , &ignore,
+
+		      TINO_GETOPT_FLAG
+		      "l	overLapping mode for -m (-m defaults to 1 MiB)\n"
+		      "		Outputs 1-12-23-34+4=1234 (triple effort).\n"
+		      "		The partial HASHes overlap by 1 block of size -m"
+		      , &overlap,
+
+		      TINO_GETOPT_ULLONG
+		      TINO_GETOPT_SUFFIX
+		      "m size	Max size of block for md5 (default: unlimited)\n"
+		      "		One MD5 sum each size bytes (and one for all).\n"
+		      "		Outputs 1+2+3+4=1234. (double effort)\n"
+		      , &maxsize,
 
 		      TINO_GETOPT_FLAG
 		      "n	read NUL terminated lines\n"
@@ -302,11 +468,16 @@ main(int argc, char **argv)
 
   if (argn<=0)
     return 1;
+
+  if (overlap && !maxsize)
+    maxsize = 1024 * 1024;
+
   if (stdinflag && direct)
     {
       tino_err("Warning: Options -d and -s together makes no sense");
       return 1;
     }
+  out = cat ? stderr : stdout;
   if (argn<argc)
     do
       md5(argv[argn]);
