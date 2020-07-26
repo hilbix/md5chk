83db7146553dfcbd6fa5797b6ac68134fd71b935
Valentin Hilbig <webmaster@scylla-charybdis.com>
2020-07-26 20:01:15 +0200

    md5chk.c for updated tinolib

--- md5chk.c
+++ md5chk.c
@@ -25,52 +25,6 @@
  * You should have received a copy of the GNU General Public License
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
- *
- * $Log$
- * Revision 1.17  2011-10-17 12:45:58  tino
- * Usage, options letters in caps
- *
- * Revision 1.16  2011-10-17 00:50:09  tino
- * Usage corrected
- *
- * Revision 1.15  2011-10-17 00:48:44  tino
- * Option -k
- *
- * Revision 1.14  2011-10-15 07:12:02  tino
- * cat, overlap, maxsize
- *
- * Revision 1.13  2009-07-17 01:10:17  tino
- * Library fixes
- *
- * Revision 1.12  2008-11-02 02:12:54  tino
- * Option -p
- *
- * Revision 1.11  2008-05-29 22:20:20  tino
- * SHIT prepared (but not yet functional)
- *
- * Revision 1.10  2008-05-29 18:48:58  tino
- * "make test" instead of subdir test
- *
- * Revision 1.9  2008-05-27 23:48:51  tino
- * DEL escaped, too
- *
- * Revision 1.8  2008-02-08 02:38:04  tino
- * Help options sorted
- *
- * Revision 1.7  2008-02-07 01:55:23  tino
- * Option -z and minor bugfix
- *
- * Revision 1.6  2007-01-17 01:19:12  tino
- * Options -s and -d
- *
- * Revision 1.5  2006/08/01 00:18:27  tino
- * The MD5 digest was printed 1 byte too short, WHOOPS.
- *
- * Revision 1.4  2006/07/31 21:24:06  tino
- * Documentation clarified (was buggy) and help (-h) improved
- *
- * Revision 1.2  2006/07/25 21:31:21  tino
- * Added commandline usage (files and -q)
  */
 
 #define TINO_NEED_OLD_ERR_FN
@@ -111,7 +65,7 @@ md5init(int i)
       snprintf(tmp, sizeof tmp, "%020u", blocknumber);
       tino_md5_update(ctx+i, tmp, 20);
       if (!++blocknumber)
-	blocknumber++;
+        blocknumber++;
     }
   if (prefix)
     tino_md5_update(ctx+i, prefix, strlen(prefix));
@@ -128,7 +82,7 @@ md5upd(const void *ptr, size_t len)
     {
       fwrite(ptr, 1, len, stdout);
       if (unbuffered)
-	fflush(stdout);
+        fflush(stdout);
     }
 }
 
@@ -184,10 +138,10 @@ md5read(FILE *fd)
     {
       md5upd(block, got);
       if (!maxsize)
-	continue;
+        continue;
       len -= got;
       if (blk>len)
-	blk = len;
+        blk = len;
     }
   if (blk)
     return 0;	/* EOF	*/
@@ -211,14 +165,14 @@ shellescapename(const char *s)
       fprintf(out, "\\%03o", (unsigned char)*s);
     else
       switch (*s)
-	{
-	case '\\':
-	case '\'':
-	  fputc('\\', out);
-	default:
-	  fputc(*s, out);
-	  break;
-	}
+        {
+        case '\\':
+        case '\'':
+          fputc('\\', out);
+        default:
+          fputc(*s, out);
+          break;
+        }
 }
 
 static void
@@ -256,81 +210,81 @@ md5(const char *name)
   else
     {
       if (stdinflag && !strcmp(name, "-"))
-	fd	= stdin;
+        fd	= stdin;
       else if ((fd=fopen(name, "rb"))==NULL)
-	{
-	  tino_err("cannot open: %s", name);
-	  return;
-	}
+        {
+          tino_err("cannot open: %s", name);
+          return;
+        }
       effort = 0;
       md5init(0);
       while (md5read(fd))
-	{
-	  if (!effort)
-	    {
-	      /* first block of -m
-	       */
-	      effort = 1;
-	      /* copy the first block so we can output it
-	       */
-	      md5copy(0,1);
-	      md5exit(1);
-	      md5init(1);
-	      continue;
-	    }
+        {
+          if (!effort)
+            {
+              /* first block of -m
+               */
+              effort = 1;
+              /* copy the first block so we can output it
+               */
+              md5copy(0,1);
+              md5exit(1);
+              md5init(1);
+              continue;
+            }
 
-	  /* already saw output	*/
-	  if (!overlap)
-	    {
-	      /* append next block	*/
-	      fputc('+', out);
-	      md5exit(1);
-	      md5init(1);
-	      continue;
-	    }
+          /* already saw output	*/
+          if (!overlap)
+            {
+              /* append next block	*/
+              fputc('+', out);
+              md5exit(1);
+              md5init(1);
+              continue;
+            }
 
-	  /* overlapping case	*/
-	  if (effort==1)
-	    {
-	      /* first 2 blocks	*/
-	      effort = 2;
-	      md5copy(0,2);
-	    }
+          /* overlapping case	*/
+          if (effort==1)
+            {
+              /* first 2 blocks	*/
+              effort = 2;
+              md5copy(0,2);
+            }
 
-	  fputc('-', out);
-	  md5exit(2);
-	  md5copy(1,2);
-	  md5init(1);
-	  continue;
-	}
+          fputc('-', out);
+          md5exit(2);
+          md5copy(1,2);
+          md5init(1);
+          continue;
+        }
       if (ferror(fd))
-	{
-	  errterm();
-	  tino_err("read error: %s", name);
-	  fclose(fd);
-	  return;
-	}
+        {
+          errterm();
+          tino_err("read error: %s", name);
+          fclose(fd);
+          return;
+        }
       if (fclose(fd))
-	{
-	  errterm();
-	  tino_err("cannot close: %s", name);
-	  return;
-	}
+        {
+          errterm();
+          tino_err("cannot close: %s", name);
+          return;
+        }
       if (effort)
-	{
-	  /* already saw output	*/
-	  if (effort==2)
-	    {
-	      fputc('-', out);
-	      md5exit(2);
-	    }
-	  /* special degraded case is handled correctly, too:
-	   * 12 => 1+2=12 ("+2=" is output next)
-	   */
-	  fputc('+', out);
-	  md5exit(1);
-	  fputc('=', out);
-	}
+        {
+          /* already saw output	*/
+          if (effort==2)
+            {
+              fputc('-', out);
+              md5exit(2);
+            }
+          /* special degraded case is handled correctly, too:
+           * 12 => 1+2=12 ("+2=" is output next)
+           */
+          fputc('+', out);
+          md5exit(1);
+          fputc('=', out);
+        }
       /* output complete hash	*/
       md5exit(0);
     }
@@ -338,9 +292,9 @@ md5(const char *name)
     {
       fputc(' ', out);
       if (zero)
-	fprintf(out, "%s", name);
+        fprintf(out, "%s", name);
       else
-	shellescapename(name);
+        shellescapename(name);
     }
   term();
 }
@@ -357,7 +311,7 @@ md5chk(void)
       return;
     }
   tino_buf_initO(&buf);
-  while ((name=tino_buf_line_read(&buf, 0, tchar ? tchar : nflag ? 0 : -1))!=0)
+  while ((name=tino_buf_line_readE(&buf, 0, tchar ? tchar : nflag ? 0 : -1))!=0)
     md5(name);
 }
 
@@ -403,104 +357,104 @@ main(int argc, char **argv)
 
   tino_verror_fn	= verror_fn;
   argn	= tino_getopt(argc, argv, 0, -1,
-		      TINO_GETOPT_VERSION(MD5CHK_VERSION)
-		      " [files..]\n"
-		      "Prototype 0:\n"
-		      "\tmd5chk -d 'string1' 'string2'\n"
-		      "\techo test | md5chk -s\n"
-		      "Prototype 1:\n"
-		      "\tif md5=\"`md5chk -iq \"$name\"`\"; then ...\n"
-		      "Prototype 2:\n"
-		      "\tfind . -type f | ./md5chk -t10\n"
-		      "Prototype 3:\n"
-		      "\tfind . -type f -print0 |\n"
-		      "\t%s -n |\n"
-		      "\twhile read -r md5 name\n"
-		      "\tdo\n"
-		      "\t\teval name=\"\\$\'$name\'\"\n"
-		      "\t\t...\n"
-		      "\tdone"
-		      ,
+                      TINO_GETOPT_VERSION(MD5CHK_VERSION)
+                      " [files..]\n"
+                      "Prototype 0:\n"
+                      "\tmd5chk -d 'string1' 'string2'\n"
+                      "\techo test | md5chk -s\n"
+                      "Prototype 1:\n"
+                      "\tif md5=\"`md5chk -iq \"$name\"`\"; then ...\n"
+                      "Prototype 2:\n"
+                      "\tfind . -type f | ./md5chk -t10\n"
+                      "Prototype 3:\n"
+                      "\tfind . -type f -print0 |\n"
+                      "\t%s -n |\n"
+                      "\twhile read -r md5 name\n"
+                      "\tdo\n"
+                      "\t\teval name=\"\\$\'$name\'\"\n"
+                      "\t\t...\n"
+                      "\tdone"
+                      ,
 
-		      TINO_GETOPT_IGNORE TINO_GETOPT_LLOPT TINO_GETOPT_FN
-		      "shit	Shell Helper Integrated Transfer (do not use)"
-		      , shit_mode,
+                      TINO_GETOPT_IGNORE TINO_GETOPT_LLOPT TINO_GETOPT_FN
+                      "shit	Shell Helper Integrated Transfer (do not use)"
+                      , shit_mode,
 
-		      TINO_GETOPT_USAGE
-		      "h	this help"
-		      ,
+                      TINO_GETOPT_USAGE
+                      "h	this help"
+                      ,
 
-		      TINO_GETOPT_UNSIGNED
-		      TINO_GETOPT_DEFAULT
-		      TINO_GETOPT_SUFFIX
-		      "b size	Blocksize for operation"
-		      , &blocksize,
-		      (unsigned)(BUFSIZ*10),
+                      TINO_GETOPT_UNSIGNED
+                      TINO_GETOPT_DEFAULT
+                      TINO_GETOPT_SUFFIX
+                      "b size	Blocksize for operation"
+                      , &blocksize,
+                      (unsigned)(BUFSIZ*10),
 
-		      TINO_GETOPT_FLAG
-		      "c	Cat mode, echo input to stdout again\n"
-		      "		sends MD5 sum to stderr, use -u to use 2>&1"
-		      , &cat,
+                      TINO_GETOPT_FLAG
+                      "c	Cat mode, echo input to stdout again\n"
+                      "		sends MD5 sum to stderr, use -u to use 2>&1"
+                      , &cat,
 
-		      TINO_GETOPT_FLAG
-		      "d	Do md5sum of commandline args or lines from stdin"
-		      , &direct,
+                      TINO_GETOPT_FLAG
+                      "d	Do md5sum of commandline args or lines from stdin"
+                      , &direct,
 
-		      TINO_GETOPT_FLAG
-		      "i	Ignore errors silently"
-		      , &ignore,
+                      TINO_GETOPT_FLAG
+                      "i	Ignore errors silently"
+                      , &ignore,
 
-		      TINO_GETOPT_FLAG
-		      "k	prefix MD5 with blocKnumbers.  Implies -m\n"
-		      "		This way equal blocks give different hashes."
-		      , &blocknumber,
+                      TINO_GETOPT_FLAG
+                      "k	prefix MD5 with blocKnumbers.  Implies -m\n"
+                      "		This way equal blocks give different hashes."
+                      , &blocknumber,
 
-		      TINO_GETOPT_FLAG
-		      "l	overLapping mode for -m (-m defaults to 1 MiB)\n"
-		      "		Outputs 1-12-23-34+4=1234 (triple effort).\n"
-		      "		The partial HASHes overlap by 1 block of size -m"
-		      , &overlap,
+                      TINO_GETOPT_FLAG
+                      "l	overLapping mode for -m (-m defaults to 1 MiB)\n"
+                      "		Outputs 1-12-23-34+4=1234 (triple effort).\n"
+                      "		The partial HASHes overlap by 1 block of size -m"
+                      , &overlap,
 
-		      TINO_GETOPT_ULLONG
-		      TINO_GETOPT_SUFFIX
-		      "m size	Max size of block for md5 (default: unlimited)\n"
-		      "		One MD5 sum each size bytes (and one for all).\n"
-		      "		Outputs 1+2+3+4=1234. (double effort)"
-		      , &maxsize,
+                      TINO_GETOPT_ULLONG
+                      TINO_GETOPT_SUFFIX
+                      "m size	Max size of block for md5 (default: unlimited)\n"
+                      "		One MD5 sum each size bytes (and one for all).\n"
+                      "		Outputs 1+2+3+4=1234. (double effort)"
+                      , &maxsize,
 
-		      TINO_GETOPT_FLAG
-		      "n	read NUL terminated lines\n"
-		      "		Note that NUL always acts as line terminator."
-		      , &nflag,
+                      TINO_GETOPT_FLAG
+                      "n	read NUL terminated lines\n"
+                      "		Note that NUL always acts as line terminator."
+                      , &nflag,
 
-		      TINO_GETOPT_STRING
-		      "p str	Preset md5 algorithm with given string\n"
-		      "		This modifies the md5 algorithm by prefixing str."
-		      , &prefix,
+                      TINO_GETOPT_STRING
+                      "p str	Preset md5 algorithm with given string\n"
+                      "		This modifies the md5 algorithm by prefixing str."
+                      , &prefix,
 
-		      TINO_GETOPT_FLAG
-		      "q	Quiet mode: do not print (shell escaped) file names"
-		      , &quiet,
+                      TINO_GETOPT_FLAG
+                      "q	Quiet mode: do not print (shell escaped) file names"
+                      , &quiet,
 
-		      TINO_GETOPT_FLAG
-		      "s	read data from Stdin instead, not a file list\n"
-		      "		Enables '-' as file argument for stdin, too."
-		      , &stdinflag,
+                      TINO_GETOPT_FLAG
+                      "s	read data from Stdin instead, not a file list\n"
+                      "		Enables '-' as file argument for stdin, too."
+                      , &stdinflag,
 
-		      TINO_GETOPT_CHAR
-		      "t	line Termination character, default whitespace\n"
-		      "		Note: -t defaults to NUL if -n present."
-		      , &tchar,
+                      TINO_GETOPT_CHAR
+                      "t	line Termination character, default whitespace\n"
+                      "		Note: -t defaults to NUL if -n present."
+                      , &tchar,
 
-		      TINO_GETOPT_FLAG
-		      "u	Unbuffered output"
-		      , &unbuffered,
+                      TINO_GETOPT_FLAG
+                      "u	Unbuffered output"
+                      , &unbuffered,
 
-		      TINO_GETOPT_FLAG
-		      "z	Write NUL(\"zero\") terminated lines, disables shell escape"
-		      , &zero,
+                      TINO_GETOPT_FLAG
+                      "z	Write NUL(\"zero\") terminated lines, disables shell escape"
+                      , &zero,
 
-		      NULL);
+                      NULL);
 
   if (argn<=0)
     return 1;
