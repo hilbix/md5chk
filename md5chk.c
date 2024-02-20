fc57b91e57a3ced2a9836b7c53cef4a96f99ddf1
Valentin Hilbig <webmaster@scylla-charybdis.com>
2024-02-20 03:56:11 +0100

    BUGFIX reset blocknumbers between files
    Before the blocknumber was not reset,
    giving unpredictable results for the 2nd and later files
    or arguments, what is bullshit.

--- md5chk.c
+++ md5chk.c
@@ -1,5 +1,4 @@
-/* $Header$
- *
+/*
  * Create shell readable MD5 lists.
  * 
  * find . -type f -print0 |
@@ -10,21 +9,8 @@
  *	...
  * done
  *
- * Copyright (C)2006-2011 Valentin Hilbig <webmaster@scylla-charybdis.com>
- *
- * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 2 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program; if not, write to the Free Software
- * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ * This Works is placed under the terms of the Copyright Less License,
+ * see file COPYRIGHT.CLL.  USE AT OWN RISK, ABSOLUTELY NO WARRANTY.
  */
 
 #define TINO_NEED_OLD_ERR_FN
@@ -62,6 +48,8 @@ md5init(int i)
     {
       char tmp[22];
 
+      if (!i)
+        blocknumber = 1;
       snprintf(tmp, sizeof tmp, "%020u", blocknumber);
       tino_md5_update(ctx+i, tmp, 20);
       if (!++blocknumber)
